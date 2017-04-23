fecb75adb60635c9d9d29b4ef664a1f045d0a2ec
math <mathob.jehanno@hotmail.fr>
2017-04-23 17:08:25 +0200

    feat : implement progression's route

---
+++ src/routes/progressRoute.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+
+});
+
+router.post("/", (req, res) => {
+
+});
+
+router.put("/", (req, res) => {
+
+});
+
+module.exports = router;
