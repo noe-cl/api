0c8101e69718be852d15353fae55aa8e538deacc
Supamiu <contact@flavien-normand.fr>
2017-05-08 13:50:31 +0200

    refactor: updated getAll decorator to follow endpoint changes.

--- src/core/decorator/get-all.ts
+++ src/core/decorator/get-all.ts
@@ -1,6 +1,6 @@
 /**
  * Created by Miu on 29/04/2017.
  */
-export function GetAll(target: any, method: string, descriptor: TypedPropertyDescriptor<() => any[]>) {
+export function GetAll(target: any, method: string, descriptor: TypedPropertyDescriptor<() => Promise<any[]>|any[]>) {
     Reflect.defineMetadata('getAll', method, target);
 }
