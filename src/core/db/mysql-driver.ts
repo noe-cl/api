00f2aefff145bfad5880a6d5cddf7e8c623eb037
Supamiu <contact@flavien-normand.fr>
2017-05-10 16:59:01 +0200

    fix(MysqlDriver): handling connection timeout error.

--- src/core/db/mysql-driver.ts
+++ src/core/db/mysql-driver.ts
@@ -2,6 +2,7 @@ import * as mysql from "mysql";
 import { Injectable } from "../decorator/injectable";
 import { Config } from "../../config/config";
 import { Promise } from "es6-promise";
+import { IError } from "mysql";
 
 
 @Injectable
@@ -9,14 +10,27 @@ export class MysqlDriver {
     private connection: mysql.IConnection;
 
     constructor(private config: Config) {
-        if (!config.data.testing) {
-            this.connection = mysql.createConnection(config.data.db);
+        this.connect();
+    }
+
+    private connect(): void {
+        if (!this.config.data.testing) {
+            this.connection = mysql.createConnection(this.config.data.db);
 
             this.connection.connect((err) => {
                 if (err) {
-                    console.log("Config file loaded from : " + config.path);
-                    console.log(JSON.stringify(config.data));
+                    console.log("Config file loaded from : " + this.config.path);
+                    console.log(JSON.stringify(this.config.data));
                     throw new Error("Database Connection error : " + err);
+                }
+            });
+
+            this.connection.on("error", (error: IError) => {
+                console.log('DB ERROR', error);
+                if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+                    this.connect();
+                } else {
+                    throw error;
                 }
             });
         }
