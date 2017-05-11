283d27e0d7185f9d08b52dab98119663e4392a4d
Supamiu <contact@flavien-normand.fr>
2017-05-11 09:43:53 +0200

    fix: users DELETE.

--- src/endpoint/auth/auth-endpoint.ts
+++ src/endpoint/auth/auth-endpoint.ts
@@ -20,11 +20,10 @@ export class AuthEndpoint {
 
     @Post()
     post(data: any): Promise<{token: string}> {
-        if (data.login === undefined || data.password === undefined) {
-            console.log("auth 400 : " + JSON.stringify(data));
-            throw new APIError(400, "Bad Request, missing property " + data.login === undefined ? "login" : "password");
-        }
-        return new Promise<{token: string}>((resolve) => {
+        return new Promise<{token: string}>((resolve, reject) => {
+            if (data.login === undefined || data.password === undefined) {
+                reject(new APIError(400, "Bad Request"));
+            }
             this.db.query("Select * from users WHERE login = ? AND password = ?", [data.login, sha(data.password)]).then(results => {
                 if (results.length === 0) {
                     throw new APIError(400, "Bad credentials.");
